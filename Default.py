f0c9efacb9804480459d42de481f4fc4e285f87a
codecrimson <mohamed.nur@codecrimson.com>
2014-01-12 16:13:07 -0500

    Initial commit

---
+++ Default.py
@@ -0,0 +1,201 @@
+import os,urllib,urllib2,re,xbmcplugin,xbmcgui,xbmcaddon,weblogin,cookielib
+
+#Bayyinah TV - Code Crimson Inc
+
+__addonname__ = 'plugin.video.bayyinahtv'
+__addon__ = xbmcaddon.Addon()
+__datapath__ = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode("utf-8")
+__icon__ = __addon__.getAddonInfo('icon')
+__cookiepath__ = os.path.join(__datapath__,'cookies.lwp')
+__username__ = ''
+__password__ = ''
+
+#the header used to pretend you are a browser
+header_string = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
+
+#initiate the cookielib class
+cj = cookielib.LWPCookieJar()
+
+def AUTHCHECK():
+        logged_in = weblogin.check_if_logged_in(__datapath__)
+        if logged_in == False:
+                logged_in = weblogin.doLogin(__cookiepath__,__username__,__password__)
+            
+        if logged_in == True:
+                cj.load(__cookiepath__)
+      
+def CATEGORIES():                
+        addDir('COURSES','http://www.bayyinah.tv/categories/137959',1,'http://s3.amazonaws.com/kajabi-media/assets/category_images/137959/small/4_Series.jpg?1372522400')
+        addDir('RAMADAN','http://www.bayyinah.tv/categories/137658',1,'http://s3.amazonaws.com/kajabi-media/assets/category_images/137658/small/bonus.png?1372357554')
+        addDir('EXTRAS','http://www.bayyinah.tv/categories/138762',1,'http://s3.amazonaws.com/kajabi-media/assets/category_images/138762/small/icon-universe.png?1372890269')
+                       
+def INDEX(url):
+        AUTHCHECK()        
+        req = urllib2.Request(url)
+        req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        req.add_header('Accept-Language','en-GB,en-US;q=0.8,en;q=0.6')
+        req.add_header('Connection','keep-alive')
+        req.add_header('Host','www.bayyinah.tv')		
+        req.add_header('User-Agent',header_string)
+        opener = urllib2.build_opener(urllib2.HTTPRedirectHandler(),urllib2.HTTPHandler(),urllib2.HTTPSHandler(),urllib2.HTTPCookieProcessor(cj))
+        response = opener.open(req)
+        source = response.read()
+        response.close()
+        match=re.compile(b'src="(.+?)" width="64" />\n  </div>\n\t<div class="list-details">\n\t\t<div class="list-title"><a href="(.+?)">(.+?)</a> </div>').findall(source)                
+        print(match)
+        for thumbnail,url,name in match:
+                addDir(name,'http://www.bayyinah.tv'+url,2,thumbnail)
+
+def INDEX2(url):
+        AUTHCHECK()        
+        req = urllib2.Request(url)
+        req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        req.add_header('Accept-Language','en-GB,en-US;q=0.8,en;q=0.6')
+        req.add_header('Connection','keep-alive')
+        req.add_header('Host','www.bayyinah.tv')		
+        req.add_header('User-Agent',header_string)
+        opener = urllib2.build_opener(urllib2.HTTPRedirectHandler(),urllib2.HTTPHandler(),urllib2.HTTPSHandler(),urllib2.HTTPCookieProcessor(cj))
+        response = opener.open(req)
+        source = response.read()
+        response.close()
+        match=re.compile(b'src="(.+?)" width="64" />\n  </div>\n\t<div class="list-details">\n\t\t<div class="list-title"><a href="(.+?)">(.+?)</a> </div>').findall(source)                
+        print(match)
+        for thumbnail,url,name in match:
+                addDir(name,'http://www.bayyinah.tv'+url,3,thumbnail)
+
+def VIDEOLINKS(url,name):
+        AUTHCHECK()        
+        req = urllib2.Request(url)
+        req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        req.add_header('Accept-Language','en-GB,en-US;q=0.8,en;q=0.6')
+        req.add_header('Connection','keep-alive')
+        req.add_header('Host','www.bayyinah.tv')		
+        req.add_header('User-Agent',header_string)
+        opener = urllib2.build_opener(urllib2.HTTPRedirectHandler(),urllib2.HTTPHandler(),urllib2.HTTPSHandler(),urllib2.HTTPCookieProcessor(cj))
+        response = opener.open(req)
+        source = response.read()
+        response.close()
+        match=re.compile(b'<div class="list-thumb"><a href="(.+?)">*<span style="display:block;width:122px;height:74px;"><img alt="" src="(.+?)" /></span>*</a>*</div>*<div class="list-details">*<div class="list-title">*<a href="(.+?)">(.+?)</a>*</div>*<div class="list-info">*').findall(source)
+        print(match)
+        for title,thumbnail,url,name in match:
+                addLink(name,'http://www.bayyinah.tv'+url,3,thumbnail)
+        
+
+                
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param
+
+
+
+
+def addLink(name,url,iconimage):
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+        return ok
+
+
+def addDir(name,url,mode,iconimage):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+
+def Notify(title,message,times,icon):
+        xbmc.executebuiltin("XBMC.Notification("+title+","+message+","+times+","+icon+")")
+
+def LOGIN(username,password):
+        uc = username[0].upper() + username[1:]
+        lc = username.lower()
+
+        logged_in = weblogin.check_if_logged_in(__datapath__)
+        if logged_in == False:
+            logged_in = weblogin.doLogin(__datapath__,username,password)
+            
+        if logged_in == True:
+                cookiepath = os.path.join(__datapath__,'cookies.lwp')
+                cj.load(cookiepath)
+                
+                Notify('Welcome back','Successfully logged into Bayyinah.tv','4000',__icon__)
+
+                addDir('COURSES','http://www.bayyinah.tv/categories/137959',1,'http://s3.amazonaws.com/kajabi-media/assets/category_images/137959/small/4_Series.jpg?1372522400')
+                addDir('RAMADAN','http://www.bayyinah.tv/categories/137658',1,'http://s3.amazonaws.com/kajabi-media/assets/category_images/137658/small/bonus.png?1372357554')
+                addDir('EXTRAS','http://www.bayyinah.tv/categories/138762',1,'http://s3.amazonaws.com/kajabi-media/assets/category_images/138762/small/icon-universe.png?1372890269')
+        elif logged_in == False:
+                Notify('Login Failure',' could not login','4000',__icon__)
+
+def STARTUP_ROUTINES():
+        #deal with bug that happens if the datapath doesn't exist
+        if not os.path.exists(__datapath__):
+          os.makedirs(__datapath__)
+
+        usrsettings = xbmcaddon.Addon(id=__addonname__)
+
+        #get username and password and do login with them
+        #also get whether to hid successful login notification
+        __username__ = usrsettings.getSetting('username')
+        __password__ = usrsettings.getSetting('password')
+
+        LOGIN(__username__,__password__) 
+        
+              
+params=get_params()
+url=None
+name=None
+mode=None
+
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        mode=int(params["mode"])
+except:
+        pass
+
+print ("Mode: "+str(mode))
+print ("URL: "+str(url))
+print ("Name: "+str(name))
+
+if mode==None or url==None or len(url)<1:
+        print ("")
+        STARTUP_ROUTINES()
+        #CATEGORIES()
+       
+elif mode==1:
+        print (""+url)
+        INDEX(url)
+        
+elif mode==2:
+        print (""+url)
+        INDEX2(url)
+        
+elif mode==3:
+        print (""+url)
+        VIDEOLINKS(url,name)
+
+
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
---
+++ weblogin.py
@@ -0,0 +1,161 @@
+# -*- coding: UTF-8 -*-
+
+"""
+ weblogin
+ by Anarchintosh @ xbmcforums
+ Copyleft (GNU GPL v3) 2011 onwards
+
+ this example is configured for Fantasti.cc login
+ See for the full guide please visit:
+ http://forum.xbmc.org/showthread.php?p=772597#post772597
+
+
+ USAGE:
+ in your default.py put:
+
+ import weblogin
+ logged_in = weblogin.doLogin('a-path-to-save-the-cookie-to','the-username','the-password')
+
+ logged_in will then be either True or False depending on whether the login was successful.
+"""
+
+import os
+import re
+import urllib,urllib2
+import cookielib
+
+### TESTING SETTINGS (will only be used when running this file independent of your addon)
+# Remember to clear these after you are finished testing,
+# so that your sensitive details are not in your source code.
+# These are only used in the:  if __name__ == "__main__"   thing at the bottom of this script.
+
+#initiate the cookielib class
+cj = cookielib.LWPCookieJar()
+        
+#install cookielib into the url opener, so that cookies are handled
+opener = urllib2.build_opener(urllib2.HTTPRedirectHandler(),
+    urllib2.HTTPHandler(),urllib2.HTTPSHandler(),urllib2.HTTPCookieProcessor(cj))
+
+#the header used to pretend you are a browser
+header_string = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
+
+#note, the cookie will be saved to the same directory as weblogin.py when testing
+
+
+def check_login(source):
+    
+    #the string you will use to check if the login is successful.
+    #you may want to set it to:    username     (no quotes)
+    logged_in_string = b'Welcome to Bayyinah TV'
+
+    #search for the string in the html, without caring about upper or lower case
+    if re.search(logged_in_string,source,re.IGNORECASE):
+        return True
+    else:
+        return False
+
+def check_if_logged_in(cookiepath):
+
+#check if user has supplied only a folder path, or a full path
+    if not os.path.isfile(cookiepath):
+        #if the user supplied only a folder path, append on to the end of the path a filename.
+        cookiepath = os.path.join(cookiepath,'cookies.lwp')
+
+    if os.path.exists(cookiepath) == False:
+        return False
+    
+    cj.load(cookiepath)
+    req = urllib2.Request('http://www.bayyinah.tv/dashboard')
+    req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+    req.add_header('Accept-Language','en-GB,en-US;q=0.8,en;q=0.6')
+    req.add_header('Connection','keep-alive')
+    req.add_header('Host','www.bayyinah.tv')		
+    req.add_header('User-Agent',header_string)
+    response = opener.open(req)
+    source = response.read()
+    response.close()		
+
+    #check the received html for a string that will tell us if the user is logged in
+    #pass the username, which can be used to do this.
+    login = check_login(source)
+
+    return login
+
+
+def doLogin(cookiepath,username, password):
+    #check if user has supplied only a folder path, or a full path
+    if not os.path.isfile(cookiepath):
+        #if the user supplied only a folder path, append on to the end of the path a filename.
+        cookiepath = os.path.join(cookiepath,'cookies.lwp')
+        
+    #delete any old version of the cookie file
+    try:
+        os.remove(cookiepath)
+    except:
+        pass
+
+    if username and password:
+
+        #the url you will request to.
+        login_url = 'http://www.bayyinah.tv/user_sessions'
+ 	
+        #build the request we will make
+        req = urllib2.Request('http://www.bayyinah.tv/login')
+        req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        req.add_header('Accept-Language','en-GB,en-US;q=0.8,en;q=0.6')
+        req.add_header('Connection','keep-alive')
+        req.add_header('Host','www.bayyinah.tv')		
+        req.add_header('User-Agent',header_string)	
+        
+        response = opener.open(req)
+        source = response.read()
+        print(source)
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(source)
+        token = soup.find("input", { 'name': 'authenticity_token'})
+		
+       
+        #build the form data necessary for the login
+        login_data = urllib.urlencode({'authenticity_token':token['value'], 'user_session[email]':username,
+                                             'user_session[password]':password, 'user_session[remember_me]':0,'x':57,'y':22})
+        binary_data = login_data.encode('ascii')
+		
+        #build the request we will make
+        req = urllib2.Request(login_url, binary_data)
+        req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        req.add_header('Accept-Language','en-GB,en-US;q=0.8,en;q=0.6')
+        req.add_header('Connection','keep-alive')
+        req.add_header('Host','www.bayyinah.tv')		
+        req.add_header('User-Agent',header_string)
+        	
+		
+        #do the login and get the response
+        print('Sending login request')
+        response = opener.open(req) 
+        source = response.read()
+        print('Login request made')
+
+        req = urllib2.Request('http://www.bayyinah.tv/dashboard')
+        req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        req.add_header('Accept-Language','en-GB,en-US;q=0.8,en;q=0.6')
+        req.add_header('Connection','keep-alive')
+        req.add_header('Host','www.bayyinah.tv')		
+        req.add_header('User-Agent',header_string)
+        response = opener.open(req)
+        source = response.read()
+        print(source)
+        response.close()		
+
+        #check the received html for a string that will tell us if the user is logged in
+        #pass the username, which can be used to do this.
+        login = check_login(source)
+
+        #if login suceeded, save the cookiejar to disk
+        if login == True:
+            cj.save(cookiepath)
+
+        #return whether we are logged in or not
+        return login
+    
+    else:
+        return False
